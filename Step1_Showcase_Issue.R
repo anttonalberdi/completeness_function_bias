d19f5da84bb984fbda8de88544e8cb737f0f5cfd
inaodr <inaodr@gmail.com>
2022-04-25 11:00:07 +0200

    commit

--- Step1_Showcase_Issue.R
+++ Step1_Showcase_Issue.R
@@ -11,7 +11,7 @@ library(sjPlot)
 
 # DRAM product
 
-Apodemus_DRAM_product=read.delim("data/DRAM/Hacked/product_Apodemus.tsv",sep="\t")
+Apodemus_DRAM_product=read.delim(gzfile("data/DRAM/Hacked/product_Apodemus.tsv.gz"),sep="\t")
 rownames(Apodemus_DRAM_product)=Apodemus_DRAM_product$genome
 Apodemus_DRAM_product=Apodemus_DRAM_product[,-1]
 # Convert True/False character to 1/0 binary numeric data
@@ -26,13 +26,16 @@ for(i in 1:ncol(Apodemus_DRAM_product)){
 # Apodemus_DRAM_product$CAZy_richness=as.integer(Apodemus_DRAM_product$CAZy_richness)
 str(Apodemus_DRAM_product)
 
-n_pathways=read.csv("data/DRAM/Hacked/pathway_numbers.csv",sep=";",header = TRUE)
+n_steps=read.csv("data/DRAM/Hacked/step_numbers.csv",sep=";",header = TRUE)
+n_steps=n_steps[match(colnames(Apodemus_DRAM_product),n_steps$ModuleID),]
+
+mean(colnames(Apodemus_DRAM_product)==n_steps$ModuleID)
 
 # Discard completely absent functions from further exploration
-Apodemus_dram=Apodemus_DRAM_product[,colSums(Apodemus_DRAM_product)>9]
-n_pathways=n_pathways[colSums(Apodemus_DRAM_product)>9,]
+Apodemus_dram=Apodemus_DRAM_product[,colSums(Apodemus_DRAM_product)>=0]
+n_steps=n_steps[colSums(Apodemus_DRAM_product)>=0,]
 
-mean(colnames(Apodemus_dram)==n_pathways$Function)
+mean(colnames(Apodemus_dram)==n_steps$ModuleID)
 
 ## BIN quality
 Apodemus_quality=read.delim("data/MAG_info/final_bins_Info_Apodemus.csv",sep=",")
@@ -58,6 +61,242 @@ Apodemus_taxmatrix$Phylum[grepl("Firmi",Apodemus_taxmatrix$Phylum)]="Firmicutes"
 
 mean(rownames(Apodemus_taxmatrix)==rownames(Apodemus_dram))
 mean(rownames(Apodemus_taxmatrix)==Apodemus_quality$genome)
+
+
+### 1.2. Crocidura dataset ###
+
+# DRAM product
+
+Crocidura_DRAM_product=read.delim(gzfile("data/DRAM/Hacked/product_Crocidura.tsv.gz"),sep="\t")
+rownames(Crocidura_DRAM_product)=Crocidura_DRAM_product$genome
+Crocidura_DRAM_product=Crocidura_DRAM_product[,-1]
+# Convert True/False character to 1/0 binary numeric data
+for(i in 1:ncol(Crocidura_DRAM_product)){
+  if(is.character(Crocidura_DRAM_product[,i])){
+    Crocidura_DRAM_product[,i][Crocidura_DRAM_product[,i]=="True"]=1
+    Crocidura_DRAM_product[,i][Crocidura_DRAM_product[,i]=="False"]=0
+    Crocidura_DRAM_product[,i]=as.integer(Crocidura_DRAM_product[,i])
+  }
+}
+# Crocidura_DRAM_product$CAZy_richness=rowSums(Crocidura_DRAM_product[,grepl("CAZy",colnames(Crocidura_DRAM_product))])
+# Crocidura_DRAM_product$CAZy_richness=as.integer(Crocidura_DRAM_product$CAZy_richness)
+str(Crocidura_DRAM_product)
+
+n_steps=read.csv("data/DRAM/Hacked/step_numbers.csv",sep=";",header = TRUE)
+n_steps=n_steps[match(colnames(Crocidura_DRAM_product),n_steps$ModuleID),]
+
+mean(colnames(Crocidura_DRAM_product)==n_steps$ModuleID)
+
+# Discard completely absent functions from further exploration
+Crocidura_dram=Crocidura_DRAM_product[,colSums(Crocidura_DRAM_product)>=0]
+n_steps=n_steps[colSums(Crocidura_DRAM_product)>=0,]
+
+mean(colnames(Crocidura_dram)==n_steps$ModuleID)
+
+## BIN quality
+Crocidura_quality=read.delim("data/MAG_info/final_bins_Info_Crocidura.csv",sep=",")
+Crocidura_quality=Crocidura_quality[Crocidura_quality$genome%in%rownames(Crocidura_dram),]
+Crocidura_quality=Crocidura_quality[match(rownames(Crocidura_dram),Crocidura_quality$genome),]
+
+## Taxonomy
+Crocidura_taxonomy=read.delim("data/GTDB_tk/gtdbtk.bac120.summary_Crocidura.tsv",sep="\t")
+Crocidura_taxonomy=Crocidura_taxonomy[match(rownames(Crocidura_dram),Crocidura_taxonomy$user_genome),]
+mean(Crocidura_taxonomy$user_genome==rownames(Crocidura_dram))
+
+Crocidura_taxonomyclean=Crocidura_taxonomy %>% 
+  mutate(classification = str_replace_all(classification, ".__", "")) %>%
+  # Split the classification string into columns for each taxonnomic rank
+  separate(col = classification, sep = ";", into = c("Domain", "Phylum", "Class", "Order", "Family", "Genus", "Species"))
+# Remove first column
+Crocidura_taxmatrix=Crocidura_taxonomyclean[,c(2:6)]
+# Set MAG name as row names of the dataframe
+rownames(Crocidura_taxmatrix)=Crocidura_taxonomyclean[,1]
+
+# Pool Firmicutes A,B C into Firmicutes
+# taxmatrix_mod=data.frame(taxmatrix)
+Crocidura_taxmatrix$Phylum[grepl("Firmi",Crocidura_taxmatrix$Phylum)]="Firmicutes"
+
+mean(rownames(Crocidura_taxmatrix)==rownames(Crocidura_dram))
+mean(rownames(Crocidura_taxmatrix)==Crocidura_quality$genome)
+
+
+### 1.3. Felis dataset ###
+
+# DRAM product
+
+Felis_DRAM_product=read.delim(gzfile("data/DRAM/Hacked/product_Felis.tsv.gz"),sep="\t")
+rownames(Felis_DRAM_product)=Felis_DRAM_product$genome
+Felis_DRAM_product=Felis_DRAM_product[,-1]
+# Convert True/False character to 1/0 binary numeric data
+for(i in 1:ncol(Felis_DRAM_product)){
+  if(is.character(Felis_DRAM_product[,i])){
+    Felis_DRAM_product[,i][Felis_DRAM_product[,i]=="True"]=1
+    Felis_DRAM_product[,i][Felis_DRAM_product[,i]=="False"]=0
+    Felis_DRAM_product[,i]=as.integer(Felis_DRAM_product[,i])
+  }
+}
+# Felis_DRAM_product$CAZy_richness=rowSums(Felis_DRAM_product[,grepl("CAZy",colnames(Felis_DRAM_product))])
+# Felis_DRAM_product$CAZy_richness=as.integer(Felis_DRAM_product$CAZy_richness)
+str(Felis_DRAM_product)
+
+n_steps=read.csv("data/DRAM/Hacked/step_numbers.csv",sep=";",header = TRUE)
+n_steps=n_steps[match(colnames(Felis_DRAM_product),n_steps$ModuleID),]
+
+mean(colnames(Felis_DRAM_product)==n_steps$ModuleID)
+
+# Discard completely absent functions from further exploration
+Felis_dram=Felis_DRAM_product[,colSums(Felis_DRAM_product)>=0]
+n_steps=n_steps[colSums(Felis_DRAM_product)>=0,]
+
+mean(colnames(Felis_dram)==n_steps$ModuleID)
+
+## BIN quality
+Felis_quality=read.delim("data/MAG_info/final_bins_Info_Felis.csv",sep=",")
+Felis_quality=Felis_quality[Felis_quality$genome%in%rownames(Felis_dram),]
+Felis_dram=Felis_dram[rownames(Felis_dram)%in%Felis_quality$genome,]
+Felis_quality=Felis_quality[match(rownames(Felis_dram),Felis_quality$genome),]
+mean(rownames(Felis_dram)==Felis_quality$genome)
+
+## Taxonomy
+Felis_taxonomy=read.delim("data/GTDB_tk/gtdbtk.bac120.summary_Felis.tsv",sep="\t")
+Felis_taxonomy=Felis_taxonomy[match(rownames(Felis_dram),Felis_taxonomy$user_genome),]
+mean(Felis_taxonomy$user_genome==rownames(Felis_dram))
+
+Felis_taxonomyclean=Felis_taxonomy %>% 
+  mutate(classification = str_replace_all(classification, ".__", "")) %>%
+  # Split the classification string into columns for each taxonnomic rank
+  separate(col = classification, sep = ";", into = c("Domain", "Phylum", "Class", "Order", "Family", "Genus", "Species"))
+# Remove first column
+Felis_taxmatrix=Felis_taxonomyclean[,c(2:6)]
+# Set MAG name as row names of the dataframe
+rownames(Felis_taxmatrix)=Felis_taxonomyclean[,1]
+
+# Pool Firmicutes A,B C into Firmicutes
+# taxmatrix_mod=data.frame(taxmatrix)
+Felis_taxmatrix$Phylum[grepl("Firmi",Felis_taxmatrix$Phylum)]="Firmicutes"
+
+mean(rownames(Felis_taxmatrix)==rownames(Felis_dram))
+mean(rownames(Felis_taxmatrix)==Felis_quality$genome)
+
+
+### 1.4. Gallus dataset ###
+
+# DRAM product
+
+Gallus_DRAM_product=read.delim(gzfile("data/DRAM/Hacked/product_Gallus.tsv.gz"),sep="\t")
+rownames(Gallus_DRAM_product)=Gallus_DRAM_product$genome
+Gallus_DRAM_product=Gallus_DRAM_product[,-1]
+# Convert True/False character to 1/0 binary numeric data
+for(i in 1:ncol(Gallus_DRAM_product)){
+  if(is.character(Gallus_DRAM_product[,i])){
+    Gallus_DRAM_product[,i][Gallus_DRAM_product[,i]=="True"]=1
+    Gallus_DRAM_product[,i][Gallus_DRAM_product[,i]=="False"]=0
+    Gallus_DRAM_product[,i]=as.integer(Gallus_DRAM_product[,i])
+  }
+}
+# Gallus_DRAM_product$CAZy_richness=rowSums(Gallus_DRAM_product[,grepl("CAZy",colnames(Gallus_DRAM_product))])
+# Gallus_DRAM_product$CAZy_richness=as.integer(Gallus_DRAM_product$CAZy_richness)
+str(Gallus_DRAM_product)
+
+n_steps=read.csv("data/DRAM/Hacked/step_numbers.csv",sep=";",header = TRUE)
+n_steps=n_steps[match(colnames(Gallus_DRAM_product),n_steps$ModuleID),]
+
+mean(colnames(Gallus_DRAM_product)==n_steps$ModuleID)
+
+# Discard completely absent functions from further exploration
+Gallus_dram=Gallus_DRAM_product[,colSums(Gallus_DRAM_product)>=0]
+n_steps=n_steps[colSums(Gallus_DRAM_product)>=0,]
+
+mean(colnames(Gallus_dram)==n_steps$ModuleID)
+
+## BIN quality
+Gallus_quality=read.delim("data/MAG_info/final_bins_Info_Gallus.csv",sep=",")
+Gallus_quality=Gallus_quality[Gallus_quality$genome%in%rownames(Gallus_dram),]
+Gallus_dram=Gallus_dram[rownames(Gallus_dram)%in%Gallus_quality$genome,]
+Gallus_quality=Gallus_quality[match(rownames(Gallus_dram),Gallus_quality$genome),]
+mean(rownames(Gallus_dram)==Gallus_quality$genome)
+
+## Taxonomy
+Gallus_taxonomy=read.delim("data/GTDB_tk/gtdbtk.bac120.summary_Gallus.tsv",sep="\t")
+Gallus_taxonomy=Gallus_taxonomy[match(rownames(Gallus_dram),Gallus_taxonomy$user_genome),]
+mean(Gallus_taxonomy$user_genome==rownames(Gallus_dram))
+
+Gallus_taxonomyclean=Gallus_taxonomy %>% 
+  mutate(classification = str_replace_all(classification, ".__", "")) %>%
+  # Split the classification string into columns for each taxonnomic rank
+  separate(col = classification, sep = ";", into = c("Domain", "Phylum", "Class", "Order", "Family", "Genus", "Species"))
+# Remove first column
+Gallus_taxmatrix=Gallus_taxonomyclean[,c(2:6)]
+# Set MAG name as row names of the dataframe
+rownames(Gallus_taxmatrix)=Gallus_taxonomyclean[,1]
+
+# Pool Firmicutes A,B C into Firmicutes
+# taxmatrix_mod=data.frame(taxmatrix)
+Gallus_taxmatrix$Phylum[grepl("Firmi",Gallus_taxmatrix$Phylum)]="Firmicutes"
+
+mean(rownames(Gallus_taxmatrix)==rownames(Gallus_dram))
+mean(rownames(Gallus_taxmatrix)==Gallus_quality$genome)
+
+
+### 1.5. Felis dataset ###
+
+# DRAM product
+
+Felis_DRAM_product=read.delim(gzfile("data/DRAM/Hacked/product_Felis.tsv.gz"),sep="\t")
+rownames(Felis_DRAM_product)=Felis_DRAM_product$genome
+Felis_DRAM_product=Felis_DRAM_product[,-1]
+# Convert True/False character to 1/0 binary numeric data
+for(i in 1:ncol(Felis_DRAM_product)){
+  if(is.character(Felis_DRAM_product[,i])){
+    Felis_DRAM_product[,i][Felis_DRAM_product[,i]=="True"]=1
+    Felis_DRAM_product[,i][Felis_DRAM_product[,i]=="False"]=0
+    Felis_DRAM_product[,i]=as.integer(Felis_DRAM_product[,i])
+  }
+}
+# Felis_DRAM_product$CAZy_richness=rowSums(Felis_DRAM_product[,grepl("CAZy",colnames(Felis_DRAM_product))])
+# Felis_DRAM_product$CAZy_richness=as.integer(Felis_DRAM_product$CAZy_richness)
+str(Felis_DRAM_product)
+
+n_steps=read.csv("data/DRAM/Hacked/step_numbers.csv",sep=";",header = TRUE)
+n_steps=n_steps[match(colnames(Felis_DRAM_product),n_steps$ModuleID),]
+
+mean(colnames(Felis_DRAM_product)==n_steps$ModuleID)
+
+# Discard completely absent functions from further exploration
+Felis_dram=Felis_DRAM_product[,colSums(Felis_DRAM_product)>=0]
+n_steps=n_steps[colSums(Felis_DRAM_product)>=0,]
+
+mean(colnames(Felis_dram)==n_steps$ModuleID)
+
+## BIN quality
+Felis_quality=read.delim("data/MAG_info/final_bins_Info_Felis.csv",sep=",")
+Felis_quality=Felis_quality[Felis_quality$genome%in%rownames(Felis_dram),]
+Felis_dram=Felis_dram[rownames(Felis_dram)%in%Felis_quality$genome,]
+Felis_quality=Felis_quality[match(rownames(Felis_dram),Felis_quality$genome),]
+mean(rownames(Felis_dram)==Felis_quality$genome)
+
+## Taxonomy
+Felis_taxonomy=read.delim("data/GTDB_tk/gtdbtk.bac120.summary_Felis.tsv",sep="\t")
+Felis_taxonomy=Felis_taxonomy[match(rownames(Felis_dram),Felis_taxonomy$user_genome),]
+mean(Felis_taxonomy$user_genome==rownames(Felis_dram))
+
+Felis_taxonomyclean=Felis_taxonomy %>% 
+  mutate(classification = str_replace_all(classification, ".__", "")) %>%
+  # Split the classification string into columns for each taxonnomic rank
+  separate(col = classification, sep = ";", into = c("Domain", "Phylum", "Class", "Order", "Family", "Genus", "Species"))
+# Remove first column
+Felis_taxmatrix=Felis_taxonomyclean[,c(2:6)]
+# Set MAG name as row names of the dataframe
+rownames(Felis_taxmatrix)=Felis_taxonomyclean[,1]
+
+# Pool Firmicutes A,B C into Firmicutes
+# taxmatrix_mod=data.frame(taxmatrix)
+Felis_taxmatrix$Phylum[grepl("Firmi",Felis_taxmatrix$Phylum)]="Firmicutes"
+
+mean(rownames(Felis_taxmatrix)==rownames(Felis_dram))
+mean(rownames(Felis_taxmatrix)==Felis_quality$genome)
+
+
 
 Explanatory_dataset=data.frame(Phylum=Apodemus_taxmatrix$Phylum,
                             completeness=Apodemus_quality$completeness-100)
